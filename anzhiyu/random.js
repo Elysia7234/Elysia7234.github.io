2bb2742cbdf3d30c120b2a63c587de165e6dc998
Elysia7234 <2637171921@qq.com>
2025-02-12 02:22:31 +0800

    Site updated: 2025-02-12 02:22:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/06/hello-world/"];function toRandomPost(){
+var posts=["2025/02/11/hello-world/","2025/02/11/浏览器去广告插件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
